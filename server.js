4252d41eb45b3dc0a8336a3a5b60bce12bc4b7cc
Yevet <xiangyiwen0218@gmail.com>
2023-08-20 01:14:05 -0700

    login and register function

--- server.js
+++ server.js
@@ -1,37 +1,73 @@
 const express = require('express');
+const mysql = require('mysql');
 const bodyParser = require('body-parser');
-const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 const port = 3000;
 
-// Create a database connection
-const db = new sqlite3.Database(':memory:');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-const mysql = require('mysql');
-
-// Create a MySQL connection
 const connection = mysql.createConnection({
   host: 'localhost', // MySQL服务器地址
-  user: 'your_username', // 用户名
-  password: 'your_password', // 密码
+  user: 'root', // 用户名
+  password: 'MY20000218', // 密码
   database: 'chatroom' // 数据库名
 });
 
-// Connect to the database
 connection.connect((err) => {
   if (err) {
     console.error('Error connecting to database:', err);
     return;
   }
   console.log('Connected to the database');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+// 根路径的路由处理
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// 用户注册和登录路由...
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    const insertQuery = `INSERT INTO users (username, password) VALUES (?, ?)`;
+  
+    connection.query(insertQuery, [username, password], (err, results) => {
+      if (err) {
+        console.error('Error registering user:', err);
+        return res.status(500).send('Error registering user');
+      }
+      console.log('User registered:', results);
+      res.status(200).send('User registered successfully');
+    });
+  });
+  
+  // 用户登录
+  app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const selectQuery = `SELECT * FROM users WHERE username = ? AND password = ?`;
+  
+    connection.query(selectQuery, [username, password], (err, results) => {
+      if (err) {
+        console.error('Error logging in:', err);
+        return res.status(500).send('Error logging in');
+      }
+  
+      if (results.length === 0) {
+        return res.status(401).send('Invalid username or password');
+      }
+  
+      console.log('User logged in:', results);
+      res.status(200).send('User logged in successfully');
+    });
+  });
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
